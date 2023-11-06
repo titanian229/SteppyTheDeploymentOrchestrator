eb432f14255ec1cf40d865b4536638f96bfe13b2
James Lee <48775473+titanian229@users.noreply.github.com>
2023-11-05 21:02:27 -0500

    Add executor registration method, add executor execute function to execute registered steps, add param validation and result validation, register CodeBuild as a sample

--- src/lambda/executor/stepTypes/codebuild.ts
+++ src/lambda/executor/stepTypes/codebuild.ts
@@ -1,1 +1,34 @@
 // Starts a CodeBuild execution
+export const type = "CodeBuild";
+
+export const handler = async (params: any) => {
+  const { CodeBuildClient, StartBuildCommand } = require("@aws-sdk/client-codebuild");
+  const client = new CodeBuildClient({ region: "ca-central-1" });
+
+  const { codeBuildName, environmentVariableOverrides = [], sourceVersion } = params;
+
+  const command = new StartBuildCommand({
+    projectName: codeBuildName,
+    sourceVersion: sourceVersion,
+    environmentVariablesOverride: environmentVariableOverrides,
+  });
+
+  const data = await client.send(command);
+
+  return data;
+};
+
+export const validator = (params: any) => {
+  const { codeBuildName, sourceVersion, environmentVariableOverrides } = params;
+
+  // TODO: Could check for permission for the step function / this Lambda to start the CodeBuild project
+  if (!codeBuildName || !sourceVersion) {
+    return false;
+  }
+
+  if (environmentVariableOverrides && !Array.isArray(environmentVariableOverrides)) {
+    return false;
+  }
+
+  return true;
+};
